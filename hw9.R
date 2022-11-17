130e6ef439b7d305713f5209e0ae3ca754048985
sbertalot <114016142+sbertalot@users.noreply.github.com>
2022-11-17 17:37:44 -0600

    Add files via upload

--- hw9.R
+++ hw9.R
@@ -4,7 +4,7 @@ set.seed(42)
 r <- 0.2
 K <- 100
 time = seq(from=1, to=50)
-N = array(dim=c(1,length(time))); N[1]=0.05*K
+N = array(dim=c(1,length(time))); N[1]=0.9*K
 for (i in time [2:length(time)]) {
   n = N[i-1]
   N[i] = n+(r*n)*(1-n/K)
@@ -46,19 +46,11 @@ optfit <- optim(par=c(16.481497, 5.038222, 100.463116), fn=obj_func)
 optcoefs <- optfit$par
 
 #################### QUESTION 4
-set.seed(42)
-r.q4 <- 0.2
-K.q4 <- 100
-time.q4 = seq(from=1, to=50)
-N.q4 = array(dim=c(1,length(time.q4))); N.q4[1]=0.05*K.q4
-for (i in time.q4 [2:length(time.q4)]) {
-  n.q4 = N.q4[i-1]
-  N.q4[i] = n.q4+(r.q4*n.q4)*(1-n.q4/K.q4)
-}
-data4 = N.q4 + rnorm(N.q4, mean = 0, sd = 0.5)
+
+N.error = N + rnorm(N, mean = 0, sd = 5)
 
 return_parameters4 <- function(x, y_pred, sigma){
-  -sum(dnorm(x=x, mean = y_pred, sd = sigma, log=TRUE))
+  -sum(dnorm(x=data4, mean = y_pred, sd = sigma, log=TRUE))
 }
 
 
@@ -73,7 +65,35 @@ obj_func4 <- function(par){
 
 
 # Estimate parameters using optim()
-optfit <- optim(par=c(0.2, 5, 100, 0.5), fn=obj_func4)
-waoptcoefs <- optfit$par
+optfit <- optim(par=c(0.2, 5, 100, 5), fn=obj_func4)
+optcoefs.4 <- optfit$par
 
-          
+
+calc_nll_new <- function(r, K, sigma, N){
+  y_pred <- (N + (r*N)*((1-N)/K))
+  nll <- return_parameters4(y_obs, y_pred, sigma)
+}
+
+x <- seq(-15,15,1)
+y <- seq(80,120, 1)
+sigma_hold <- 5
+
+# Setup grid search
+nll_matrix <- matrix(data=NA, nrow=length(x), ncol=length(y), 
+                     dimnames=list(x, y))
+# Begin grid search: i <- 1; j <- 1
+for(i in 1:length(x)){
+  x_curr <- x[i]
+  for(j in 1:length(y)){
+    y_curr <- y[j]
+    nll <- calc_nll_new(x_curr, y_curr, sigma_hold, N.error)
+    nll_matrix[i,j] <- nll
+  }
+}
+
+persp(x=x, y=y, z=nll_matrix, theta=90, phi=20)
+
+#There is a ridge along the likelihood surface coorelating with the estimated K value, the estimate r value seems to be better represented
+
+
+
